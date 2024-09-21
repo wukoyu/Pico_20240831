2c32f1d5d66089b41b7e3b36f16d5a53116b8453
wukoyu <wukoyu@gmail.com>
2024-09-21 13:35:12 +0800

    thonny

--- Pico/lesson4/main1.py
+++ Pico/lesson4/main1.py
@@ -1,1 +1,15 @@
-print("Hello! Python form micropython")
+from machine import Pin #只使用Pin Class不需import整個machine
+import time
+
+led = Pin("LED", mode=Pin.OUT)
+status = False
+while True:
+    #led.on()
+    if status == False:
+        led.on()
+        status = True
+    else:
+        led.off()
+        status = False
+    
+    time.sleep(1)
